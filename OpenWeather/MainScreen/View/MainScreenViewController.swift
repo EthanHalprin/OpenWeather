b588773b3869ae276f3699309cbd0a53b01eafe3
Ethan <ethan.halprin@gmail.com>
2021-07-13 11:04:16 +0300

    Bar button toggle for Grid/List with SF System icons

--- OpenWeather/MainScreen/View/MainScreenViewController.swift
+++ OpenWeather/MainScreen/View/MainScreenViewController.swift
@@ -11,14 +11,35 @@ class MainScreenViewController: UIViewController {
 
     var viewModel = MainScreenViewModel()
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        setup()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        viewModel.loadForecast(for: .telAviv) { forecast in
-            print("===== FORCAST TA ====================================")
-            dump(forecast)
-            print("===== FORCAST TA ====================================")
-        }
+//        viewModel.loadForecast(for: .telAviv) { forecast in
+//            print("===== FORCAST TA ====================================")
+//            dump(forecast)
+//            print("===== FORCAST TA ====================================")
+//        }
+    }
+}
+
+extension MainScreenViewController {
+    
+    fileprivate func setup() {
+        let image = viewModel.isGridLayout ? UIImage(systemName: "list.bullet") : UIImage(systemName: "square.grid.2x2")
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: image,
+                                                            style: .plain,
+                                                            target: self,
+                                                            action: #selector(layoutToggleTapped))
+    }
+    
+    @objc func layoutToggleTapped() {
+        viewModel.isGridLayout = viewModel.isGridLayout ? false : true
+        navigationItem.rightBarButtonItem!.image = viewModel.isGridLayout ? UIImage(systemName: "list.bullet") : UIImage(systemName: "square.grid.2x2")
     }
 }
 
--- OpenWeather/MainScreen/ViewModel/MainScreenViewModel.swift
+++ OpenWeather/MainScreen/ViewModel/MainScreenViewModel.swift
@@ -9,6 +9,8 @@ import Foundation
 
 class MainScreenViewModel {
     
+    var isGridLayout = true
+    
     func loadForecast(for city: City, _ completion: @escaping (Forecast) -> Void) {
         URLLoader.shared.loadForecastData(for: city) { result in
             switch result {
