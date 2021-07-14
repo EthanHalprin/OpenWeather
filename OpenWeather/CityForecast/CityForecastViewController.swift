8450dd94bb283cd62e56585a90c31c2ae3312c75
Ethan <ethan.halprin@gmail.com>
2021-07-14 23:41:01 +0300

    CityScreen VC perfected & constrainted

--- OpenWeather/CityForecast/CityForecastViewController.swift
+++ OpenWeather/CityForecast/CityForecastViewController.swift
@@ -12,9 +12,33 @@ import UIKit
 class CityForecastViewController: UIViewController {
 
     var forecast = ForecastPersist()
-        
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UITextView!
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var feelingLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    @IBOutlet weak var pressureLabel: UILabel!
+    @IBOutlet weak var seaLevelLabel: UILabel!
+    @IBOutlet weak var windSpeedLabel: UILabel!
+    @IBOutlet weak var windDirectionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        if let city = forecast.cityName {
+            imageView.image = UIImage(named: city)
+        }
+
+        nameLabel.text = forecast.cityName
+        descriptionLabel.text = forecast.weatherDescription
+        tempLabel.text = String("\(forecast.temperature)") + "º"
+        feelingLabel.text = String("\(forecast.feelsLike)") + "º"
+        humidityLabel.text = String("\(forecast.humidity)")
+        pressureLabel.text = String("\(forecast.pressure)")
+        seaLevelLabel.text = String("\(forecast.seaLevel)")
+        windSpeedLabel.text = String("\(forecast.windSpeed)")
+        windDirectionLabel.text = String("\(forecast.windDirection)") + "º"
     }
 }
